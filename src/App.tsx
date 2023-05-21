6f5c3af13bdc2e84e05f5ff78ff54e3058d5e70c
Silky Ng <silky.ng@gmail.com>
2023-05-21 19:55:57 +0100

    App with summariesList

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+import { Summaries } from './Summaries'
+import { Summary } from './interfaces/summary'
 
-function App() {
+function App () {
+  const [isEnabled, setIsEnabled] = useState<boolean>(true)
+  const [error, setError] = useState<string>('')
+  const [summariesList, setSummariesList] = useState<Summary[] | null>(null)
+
+  async function getSummaries () {
+    const backendUrl = process.env.REACT_APP_BACKEND_URL
+      ? process.env.REACT_APP_BACKEND_URL
+      : 'http://localhost:3000'
+
+    try {
+      const response = await axios.get(`${backendUrl}/openai/summaries`)
+      setSummariesList(response.data)
+    } catch (error) {
+      if (error instanceof Error) setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    if (isEnabled) getSummaries()
+  }, [isEnabled])
+
   return (
     <div className="App">
       <header>
-        Hello world!
+        OpenAI Chrome Extension
       </header>
+      <button onClick={() => setIsEnabled(!isEnabled)}>
+        {isEnabled ? 'Disable' : 'Enable'}
+      </button>
+      {error && <>
+        <p>{error}</p>
+          <button onClick={() => {
+            setError('')
+            getSummaries()
+          }}>
+            Try Again
+          </button>
+        </>}
+      {isEnabled && <Summaries summariesList={summariesList}/>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
