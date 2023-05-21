9f3a9aee279def429243b3024e573ca4bd5422b3
Silky Ng <silky.ng@gmail.com>
2023-05-21 19:54:59 +0100

    edited setup of root container

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.createElement("div")
+root.className = "container"
+document.body.appendChild(root)
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
