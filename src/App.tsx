784e5ea436fc2ed466c40ea36ebb51905ae40f49
Silky Ng <silky.ng@gmail.com>
2023-05-22 09:02:19 +0100

    mui switch and circular progress styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import axios from 'axios'
 import './App.css'
 import { Summaries } from './Summaries'
 import { Summary } from './interfaces/summary'
+import Switch from '@mui/material/Switch'
+import CircularProgress from '@mui/material/CircularProgress'
+import Box from '@mui/material/Box'
 
 function App () {
-  const [isEnabled, setIsEnabled] = useState<boolean>(true)
+  const [isEnabled, setIsEnabled] = useState<boolean>(false)
   const [error, setError] = useState<string>('')
   const [summariesList, setSummariesList] = useState<Summary[] | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   async function getSummaries () {
     const backendUrl = process.env.REACT_APP_BACKEND_URL
@@ -15,11 +19,20 @@ function App () {
       : 'http://localhost:3000'
 
     try {
+      setIsLoading(true)
       const response = await axios.get(`${backendUrl}/openai/summaries`)
       setSummariesList(response.data)
+      setIsLoading(false)
     } catch (error) {
       if (error instanceof Error) setError(error.message)
+      setIsLoading(false)
     }
+  }
+
+  function handleSwitch () {
+    setIsEnabled(!isEnabled)
+
+    setError('')
   }
 
   useEffect(() => {
@@ -31,9 +44,7 @@ function App () {
       <header>
         OpenAI Chrome Extension
       </header>
-      <button onClick={() => setIsEnabled(!isEnabled)}>
-        {isEnabled ? 'Disable' : 'Enable'}
-      </button>
+      <Switch checked={isEnabled} onChange={handleSwitch} inputProps={{ 'aria-label': 'controlled' }}/>
       {error && <>
         <p>{error}</p>
           <button onClick={() => {
@@ -43,7 +54,12 @@ function App () {
             Try Again
           </button>
         </>}
-      {isEnabled && <Summaries summariesList={summariesList}/>}
+      {isLoading &&
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <CircularProgress />
+        </Box>
+      }
+      {isEnabled && summariesList && <Summaries summariesList={summariesList}/>}
     </div>
   )
 }
