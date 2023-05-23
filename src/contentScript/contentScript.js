46c94df675e2cbbdba331af3816f68870b7e8f4b
Silky Ng <silky.ng@gmail.com>
2023-05-23 15:46:11 +0100

    fix - summarise text on selection

--- src/contentScript/contentScript.js
+++ src/contentScript/contentScript.js
@@ -1,9 +1,12 @@
 (() => {
+  const innerHTMLTooltip = '<p id="tooltip" style="text-align:center; padding: 4px">Summarise text</p>'
   const tooltip = document.createElement('button')
-  tooltip.innerHTML = '<p id="tooltip" style="text-align:center; padding: 4px">Summarise text</p>'
+  tooltip.innerHTML = innerHTMLTooltip
   tooltip.style.display = 'block'
   tooltip.style.color = 'black'
   tooltip.style.width = '300px'
+  tooltip.style.fontSize = '1.5em'
+  tooltip.style.fontFamily = 'Arial'
   tooltip.style.zIndex = '10'
   tooltip.style.position = 'absolute'
   tooltip.style.borderRadius = '8px'
@@ -12,14 +15,12 @@
   tooltip.addEventListener('click', fetchSummarisedText)
 
   function selectableTextAreaMouseUp (event) {
-    // const isEnabled = window.localStorage.getItem('openaiSummaries')
-    // console.log('isEnabled: ', isEnabled)
-    // if (isEnabled === 'false' || !isEnabled) return
     const selectedText = window.getSelection()?.toString().trim()
     if (selectedText.length && event.target.id !== 'tooltip') {
       const x = event.pageX
       const y = event.pageY
       tooltip.style.display = 'block'
+      tooltip.innerHTML = innerHTMLTooltip
       const toolTipHeight = Number(getComputedStyle(tooltip).height.slice(0, -2))
       tooltip.style.left = `${x}px`
       tooltip.style.top = `${y - toolTipHeight * 1.5}px`
