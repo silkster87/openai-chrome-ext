28f64aec199eaee19928031154754e2ad1b75d1e
Silky Ng <silky.ng@gmail.com>
2023-05-23 15:35:04 +0100

    delete icon in Summaries

--- src/Summaries.tsx
+++ src/Summaries.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Summary } from './interfaces/summary'
+import { DeleteIcon } from './icons/DeleteIcon'
+import './Summaries.css'
 
 interface IProps {
-  summariesList: Summary[] | null
+  summariesList: Summary[] | null;
+  deleteSummary: (id: string) => void;
 }
-export function Summaries ({ summariesList }: IProps) {
+export function Summaries ({ summariesList, deleteSummary }: IProps) {
   return (
     <div className="summaries-list">
       {summariesList && summariesList.map(summary => {
         const date = new Date(summary.created * 1000)
         return (
-          <>
-            <p>{`Date: ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`}</p>
+          <div key={summary._id} className="summary-container">
+            <div className="summary-top">
+              <p>{`${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`}</p>
+              <button onClick={() => deleteSummary(summary._id)} className="delete-btn">
+                <DeleteIcon />
+              </button>
+            </div>
             <p>{summary.text}</p>
-          </>
+          </div>
         )
       })}
       {!summariesList && <p>You have no summaries</p>}
